b73d4604c9fe3ff2d40d88b13c23ec073c476354
Michelle Gu <michelle.gu@utexas.edu>
2018-11-10 14:18:23 -0600

    Fixed app to run properly, add manage.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import db
+#from models import db
 
 app = Flask(__name__)
 
@@ -8,22 +8,18 @@ def main():
     return 'Hello, World!'
 
 # App config
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.config['DEBUG'] = True
-    db.init_app(app)
-    POSTGRES = {
-        'user': 'postgres',
-        'pw': 'password',
-        'db': 'my_database',
-        'host': 'localhost',
-        'port': '5432',
-    }
-    app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' \
-        % POSTGRES
-
-
-
-    
+#    db.init_app(app)
+#    POSTGRES = {
+#        'user': 'postgres',
+#        'pw': 'password',
+#        'db': 'my_database',
+#        'host': 'localhost',
+#        'port': '5432',
+#    }
+#    app.config['SQLALCHEMY_DATABASE_URI'] = \
+#        'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' \
+#        % POSTGRES       
     app.run()
 
--- models.py
+++ models.py
@@ -4,13 +4,13 @@ db = SQLAlchemy()
 
 # Define our models classes hereafter
 class BaseModel(db.Model):
-"""Base data model for all objects"""
+    """Base data model for all objects"""
     __abstract__ = True
     # define here __repr__ and json methods or any common method
     # that you need for all your models
 
 class YourModel(BaseModel):
-"""model for one of your table"""
+    """model for one of your table"""
     __tablename__ = 'my_table'
     # define your model
 
