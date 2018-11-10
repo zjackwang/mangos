66b0ca156c75d2d1f365c07f16310d9267436055
Michelle Gu <michelle.gu@utexas.edu>
2018-11-10 13:40:33 -0600

    Added a base model and config database in app

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from models import db
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return 'Hello, World!'
+
+# App config
+if __name__ = '__main__':
+    app.config['DEBUG'] = True
+    db.init_app(app)
+    POSTGRES = {
+        'user': 'postgres',
+        'pw': 'password',
+        'db': 'my_database',
+        'host': 'localhost',
+        'port': '5432',
+    }
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' \
+        % POSTGRES
+
+
+
+    
+    app.run()
 
---
+++ models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Define our models classes hereafter
+class BaseModel(db.Model):
+"""Base data model for all objects"""
+    __abstract__ = True
+    # define here __repr__ and json methods or any common method
+    # that you need for all your models
+
+class YourModel(BaseModel):
+"""model for one of your table"""
+    __tablename__ = 'my_table'
+    # define your model
+
+
